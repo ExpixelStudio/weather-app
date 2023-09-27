6bde566de07f735919b373e153feb51d09658d1a
gtlegend <yusuf_ar@live.com>
2023-09-27 09:08:23 -0400

    add function for user manual search via a search bar.

--- app.js
+++ app.js
@@ -1,17 +1,69 @@
 const key = '7bf03271313d4720a4171840231809';
+const form = document.getElementById('search-form');
+const search = document.getElementById('search');
 
-const fetchData = async () => {
+const fetchCurrentData = async () => {
     const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${key}&q=Barbados&aqi=no` ,
         {mode:'cors'}
-    )
+    );
     const data = await response.json();
         
     return data;
 }
 
-const getInfo = async () => {
+const fetchForecastData = async (location) => {
+    const response = await fetch (`http://api.weatherapi.com/v1/forecast.json?key=${key}&q=Barbados&days=3&aqi=no&alerts=no` ,
+    {mode:'cors'}
+    );
 
-    const data = await fetchData();
+    const data = await response.json();
+
+    return data;
+}
+
+const fetchSearchData = async (location) => {
+    const response = await fetch (`http://api.weatherapi.com/v1/search.json?key=${key}&q=${location}
+    ` ,
+    {mode:'cors'}
+    );
+
+    const data = await response.json();
+    console.log(data);
+    return data;
+}
+
+/* async function getSearchQuery() { */
+    
+    form.addEventListener('submit' , (e) => {
+        e.preventDefault();
+        new FormData(form); //FormData is an inbuilt prototype
+
+        let fd = new FormData(form); //fd formdata
+
+        const obj = Object.fromEntries(fd); //returns js object from data passed in
+        const json = JSON.stringify(obj);
+
+        fetchSearchData(search.value);
+        console.log(search.value);
+        console.log(obj);
+    })
+
+
+
+
+const getForecastInfo = async () => {
+
+    const data = await fetchForecastData();
+    return console.log(data);
+    
+    const info = {
+        
+    }
+}
+
+const getCurrentInfo = async () => {
+
+    const data = await fetchCurrentData();
 
     const info = {
         condition : `${data.current.condition.text}`,
@@ -24,16 +76,17 @@ const getInfo = async () => {
         wind_speed : `${data.current.gust_kph}`,
         humidity : `${data.current.humidity}`,
     };
-    console.log(data);
-    console.log(info);
+    console.log({data});
+    console.log({info});
     return info;
 }
 
-const get = fetchData();
-console.log(get);
+fetchCurrentData();
 
-getInfo().then((info) => {
-    console.log(info.condition);
+getForecastInfo();
+
+getCurrentInfo().then((info) => {
+    /* console.log(info.condition); */
 });
 
 /* const fetchData = () => {
